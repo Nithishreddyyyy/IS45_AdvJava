25df9bffe06dbc2339e6bbbdee4ee027b979b8e7
Nithish Reddy <Nithishreddyb2@gmail.com>
2025-03-24 10:19:21 +0530

    Modified String.java

--- src/Unit_2/String.java
+++ src/Unit_2/String.java
@@ -2,7 +2,9 @@ package Unit_2;
 
 class Test {
     public static void main(String[] args) {
-        java.lang.String name = "Nithish";
-        System.out.println(name);
+        java.lang.String name1 = "Nithish";
+        java.lang.String name2 = " Reddy";
+        java.lang.String name3 = name1 + name2;
+        System.out.println(name3);
     }
 }
