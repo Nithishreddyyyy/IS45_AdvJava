ce838f74cb2f43ff87539fcfa78e782df70a0b70
Nithish Reddy <Nithishreddyb2@gmail.com>
2025-03-25 12:11:10 +0530

    Update made to String.java

--- src/Unit_2/String.java
+++ src/Unit_2/String.java
@@ -3,8 +3,17 @@ package Unit_2;
 class Test {
     public static void main(String[] args) {
         java.lang.String name1 = "Nithish";
-        java.lang.String name2 = " Reddy";
-        java.lang.String name3 = name1 + name2;
+        java.lang.String name2 = " Reddy is : ";
+        int age = 10;
+        java.lang.String name3 = name1 + name2 + age;
         System.out.println(name3);
+
+        java.lang.String N = "Hello";
+        java.lang.String I = "Hello";
+        if(N.equals(I)){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not equal");
+        }
     }
 }
