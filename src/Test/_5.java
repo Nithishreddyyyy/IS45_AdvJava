75600ee4e2550afdb943b0865ac0a9b101143a89
Nithish Reddy <nithishreddyb2@gmail.com>
2025-05-23 23:45:58 +0530

    Commiting all

---
+++ src/Test/_5.java
@@ -0,0 +1,9 @@
+package Test;
+
+public class _5 {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer();
+        sb.append("Nithish");
+        System.out.println(sb);
+    }
+}
