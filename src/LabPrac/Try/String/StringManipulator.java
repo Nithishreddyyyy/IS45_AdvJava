607573a5e196525b64a6f31934e341dc0dc442be
Nithish Reddy <nithishreddyb2@gmail.com>
2025-05-26 12:15:40 +0530

    file added

---
+++ src/LabPrac/Try/String/StringManipulator.java
@@ -0,0 +1,11 @@
+package LabPrac.Try.String;
+
+public interface StringManipulator {
+    String reverse(String input);
+    String Upper(String input);
+    String Lower(String input);
+    String concatenate(String str1, String str2);
+    int countVowels(String input);
+    int wordCount(String input);
+    Boolean Palindrome(String input);
+}
