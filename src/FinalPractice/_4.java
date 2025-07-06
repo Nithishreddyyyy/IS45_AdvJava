f0ff16013c5085a0d4a5e56b2af377a47037b4be
Nithish Reddy <nithishreddyb2@gmail.com>
2025-07-06 20:34:17 +0530

    Added file

--- src/FinalPractice/_4.java
+++ src/FinalPractice/_4.java
@@ -1,4 +1,17 @@
 package FinalPractice;
 
 public class _4 {
+    private int test = 10;
 }
+class aA{
+    aA(){
+        super();
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        aA obj = new aA();
+        System.out.println(obj.res());
+    }
+}
+
