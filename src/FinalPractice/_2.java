ae9a245d3a6acc70ef57531c93e36d3c859df394
Nithish Reddy <nithishreddyb2@gmail.com>
2025-07-06 20:23:49 +0530

    added

---
+++ src/FinalPractice/_2.java
@@ -0,0 +1,13 @@
+package FinalPractice;
+
+
+public class _2 {
+    int speed;
+    static void drive(){
+        System.out.println("Driving");
+    }
+
+    public static void main(String[] args) {
+        drive();
+    }
+}
